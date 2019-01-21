5a45906e66b3b17048a8cc499254c20a01bfe4d2
韦胜健 <martsforever@aliyun.com>
2019-01-21 21:03:32 +0800

    add User module

---
+++ src/main/java/com/creditmanage/base/user/User.java
@@ -0,0 +1,61 @@
+package com.creditmanage.base.user;
+
+import com.creditmanage.core.basic.BasicModel;
+
+public class User extends BasicModel {
+
+    private String username;                        //用户名
+    private String password;                        //密码
+    private String code;                            //学号|工号
+    private String role;                            //角色
+    private String tel;                             //电话号码
+    private String email;                           //邮箱地址
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/creditmanage/base/user/UserMapper.java
@@ -0,0 +1,8 @@
+package com.creditmanage.base.user;
+
+import com.creditmanage.core.basic.BasicMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BasicMapper<User> {
+}
