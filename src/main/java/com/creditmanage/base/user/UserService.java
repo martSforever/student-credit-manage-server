3cfe01dd8059c98c6ce0c2217a9f26345a6565ac
martsforever <martsforever@aliyun.com>
2019-02-01 12:13:03 +0800

    add user logic optimize

--- src/main/java/com/creditmanage/base/user/UserService.java
+++ src/main/java/com/creditmanage/base/user/UserService.java
@@ -2,5 +2,15 @@ package com.creditmanage.base.user;
 
 import com.creditmanage.core.basic.BasicService;
 
+import java.util.Map;
+
 public interface UserService extends BasicService<User> {
+
+    /**
+     * 注册用户
+     *
+     * @param user
+     * @return
+     */
+    Map<String, Object> addUser(User user);
 }
--- src/main/java/com/creditmanage/base/user/UserServiceImpl.java
+++ src/main/java/com/creditmanage/base/user/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.creditmanage.base.user;
 
+import com.creditmanage.core.ControllerUtils;
 import com.creditmanage.core.basic.BasicMapper;
 import com.creditmanage.core.basic.BasicServiceImpl;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Map;
 
 @Service
 public class UserServiceImpl extends BasicServiceImpl<User> implements UserService {
@@ -15,5 +18,17 @@ public class UserServiceImpl extends BasicServiceImpl<User> implements UserServi
     @Override
     public BasicMapper<User> basicMapper() throws Exception {
         return userMapper;
+    }
+
+    @Override
+    public Map<String, Object> addUser(User user) {
+        if (StringUtils.isBlank(user.getTel())) {
+            return ControllerUtils.error("电话号码不能为空！");
+        }
+        String tel = user.getTel();
+        user.setPassword(tel.substring(tel.length() - 6));              //密码为电话号码后六位
+        user.setUsername(user.getCode());                               //用户名为工号/学号
+        userMapper.insert(user);
+        return ControllerUtils.success(null);
     }
 }
